8dabe2d0e14b4e760e24059570452ef76f2fd53a
Chris Rogers <ChristopherRogers1991@gmail.com>
2018-02-03 15:13:08 -0500

    Send transcribed messages to mycroft.
    With this commit, this is a working proof of concept.

--- main.py
+++ main.py
@@ -1,20 +1,37 @@
 from object_watcher.object_watcher import ObjectWatcher
 from simple_audio_recorder.simple_audio_recorder import SimpleAudioRecorder
 from stt.simple_stt import SimpleSTT
+from websocket import create_connection
+import json
 import time
 import traceback
 
 transcriber = SimpleSTT()
 
+URL_TEMPLATE = "{scheme}://{host}:{port}{path}"
 
-def transcribe_and_print(path_to_source):
+
+def send_message(message, host="localhost", port=8181, path="/core", scheme="ws"):
+    payload = json.dumps({
+        "type": "recognizer_loop:utterance",
+        "context": "",
+        "data": {
+            "utterances": [message]
+        }
+    })
+    url = URL_TEMPLATE.format(scheme=scheme, host=host, port=str(port), path=path)
+    ws = create_connection(url)
+    ws.send(payload)
+    ws.close()
+
+
+def transcribe_and_send(path_to_source):
     try:
         text = transcriber.transcribe(path_to_source)
         print(text)
+        send_message(text)
     except Exception:
         traceback.print_exc()
-
-
 
 if __name__ == "__main__":
     recorder = SimpleAudioRecorder()
@@ -26,7 +43,7 @@ if __name__ == "__main__":
 
         fw.register_object_left_callback(recorder.stop_recording)
         fw.register_object_left_callback(print, "done recording!")
-        fw.register_object_left_callback(transcribe_and_print, "output.wav")
+        fw.register_object_left_callback(transcribe_and_send, "output.wav")
 
         fw.start()
 
